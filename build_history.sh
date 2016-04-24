6138443f5322a345428807ef45bc57abac3f93a9
ianmiell <ian.miell@gmail.com>
2016-04-24 17:13:03 +0100

    docs

--- build_history.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for ((i=1;i<=1024;i++))
-do
-	echo $i >> projectfile
-	git commit -am "Commit iteration $i"
-done
--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-MD5=$(cat projectfile | md5sum)
-if [[ $MD5 = 'asd' ]]
-then
-	echo OK
-else
-	echo FAIL
-fi
